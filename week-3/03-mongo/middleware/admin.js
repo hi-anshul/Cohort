8667f053ecafa4c6294b389193464ea0d8d104be
hi-anshul <youcancodeanything@gmail.com>
2023-12-22 00:48:34 +0530

    Updated Middleware

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,6 +1,22 @@
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    
+  let username = req.headers.username;
+  let password = req.headers.password;
+
+  try {
+    const user = Admin.findOne({ username }).then((result) => {
+      result.json();
+    });
+
+    if (password === user.password) {
+      res.json({ message: "Login successful" });
+      next();
+    } else {
+      res.status(401).json({ error: "Invalid username or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 module.exports = adminMiddleware;
